f3ecd193074d6f1dfe4c07762a6b06af7514479c
stagiaire <stagiaire>
2017-02-13 11:53:13 +0100

    Mon premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Salut, comment va {{name}}?</h1>`,
+
+  styles: [`
+
+    h1{
+      color: red;
+      text-transform: uppercase;
+    }
+
+  `]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'John Smith'; }
